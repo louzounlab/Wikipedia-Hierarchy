ecfaa209c26b384b3c8f0319193a4b9a8204de20
rachelic44 <45845591+rachelic44@users.noreply.github.com>
2019-12-15 09:55:28 +0200

    Update projections.py

--- projections.py
+++ projections.py
@@ -1,5 +1,5 @@
 '''
-Use this file at first, to project the graph. dimensions can be chosen by user, changes can be applied in configuration file - conf.ini
+Use this file at first, to project the grph. dimensions can be chosen by uder, change in configuration file - conf.ini
 '''
 
 import configparser
@@ -12,18 +12,22 @@ from sklearn.manifold import Isomap
 
 config = configparser.ConfigParser()
 config.read('conf.ini')
-d_dimensions = config['ARGUMENTS']['dimensions']
+d_dimensions = int(config['ARGUMENTS']['dimensions'])
+graph_name = config['ARGUMENTS']['adjlist_name']
+projected_name = config['ARGUMENTS']['projected_graph_name']
 
 
-g=nx.read_adjlist(open("GraphAsAdjlist.adjlist", 'rb'), create_using=nx.DiGraph())
+
+
+g=nx.read_adjlist(open(graph_name, 'rb'), create_using=nx.DiGraph())
 #
 # # Precompute probabilities and generate walks - **ON WINDOWS ONLY WORKS WITH workers=1**
 node2vec = Node2Vec(g, dimensions=d_dimensions, walk_length=10, num_walks=10, workers=4)  # Use temp_folder for big graphs
 #
 # # Embed nodes
 model = node2vec.fit(window=100, min_count=1, batch_words=4)
-model.wv.save_word2vec_format("projected")
-t=open("projected")
+model.wv.save_word2vec_format(projected_name)
+t=open(projected_name)
 y=t.readlines()
 
 for i in range(1,len(y)):
